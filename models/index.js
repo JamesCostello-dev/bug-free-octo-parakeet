4f755e0a814601bd20dffe916e247045f0020fc9
James Costello <jmc@Jamess-MacBook-Pro.local>
2021-01-17 13:08:00 -0700

    fix models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
 const Comment = require('./Comment');
 
+// create associations
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -12,15 +14,36 @@ Post.belongsTo(User, {
 });
 
 User.belongsToMany(Post, {
-  through: Post,
+  through: Vote,
+  as: 'voted_posts',
+
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
 });
 
 Post.belongsToMany(User, {
-  through: Post,
+  through: Vote,
+  as: 'voted_posts',
   foreignKey: 'post_id',
   onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
 });
 
 Comment.belongsTo(User, {
@@ -42,4 +65,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Comment };
+module.exports = { User, Post, Vote, Comment };
